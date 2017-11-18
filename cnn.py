8b852655dbb6b1d6af96adda145b3e2f73d4518e
YuanYang <yangyuan95@gmail.com>
2017-11-17 21:37:13 -0800

    cnn

--- cnn.py
+++ cnn.py
@@ -162,17 +162,80 @@ def forwardNN(last_rnn_output):
     l2_loss = tf.nn.l2_loss(weights_1) + tf.nn.l2_loss(out_weights) + tf.nn.l2_loss(weights_2)
     return out_result, l2_loss
 
-lstm_cells = tf.contrib.rnn.BasicLSTMCell(num_units=lstm_size)
-outputs, state = tf.nn.dynamic_rnn(cell=lstm_cells, inputs=inputs, dtype=tf.float32, sequence_length=sentense_length)
-outputs = tf.nn.dropout(outputs, keep_prob=0.6)
-last_output_idx = tf.range(tf.shape(outputs)[0]) * tf.shape(outputs)[1] + sentense_length - 1
-last_rnn_output = tf.gather(tf.reshape(outputs, [-1, lstm_size]), last_output_idx)
+#lstm_cells = tf.contrib.rnn.BasicLSTMCell(num_units=lstm_size)
+#outputs, state = tf.nn.dynamic_rnn(cell=lstm_cells, inputs=inputs, dtype=tf.float32, sequence_length=sentense_length)
+#outputs = tf.nn.dropout(outputs, keep_prob=0.6)
+#last_output_idx = tf.range(tf.shape(outputs)[0]) * tf.shape(outputs)[1] + sentense_length - 1
+#last_rnn_output = tf.gather(tf.reshape(outputs, [-1, lstm_size]), last_output_idx)
 
-# one_batch_predict = tf.nn.softmax(tf.matmul(last_rnn_output, weights) + bias)
-one_batch_predict = forwardNN(last_rnn_output)[0]
-classify = tf.nn.sigmoid(one_batch_predict)
-probability = tf.nn.softmax(classify)
-l2_loss = forwardNN(last_rnn_output)[1]
+#================cnn===============
+# Create some wrappers for simplicity
+def conv2d(x, W, b, strides=1):
+    # Conv2D wrapper, with bias and relu activation
+    x = tf.nn.conv2d(x, W, strides=[1, strides, strides, 1], padding='SAME')
+    x = tf.nn.bias_add(x, b)
+    return tf.nn.relu(x)
+
+
+def maxpool2d(x, k=2):
+    # MaxPool2D wrapper
+    return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1],
+                          padding='SAME')
+
+
+# Create model
+def conv_net(x, weights, biases, dropout):
+    # MNIST data input is a 1-D vector of 784 features (28*28 pixels)
+    # Reshape to match picture format [Height x Width x Channel]
+    # Tensor input become 4-D: [Batch Size, Height, Width, Channel]
+    x = tf.reshape(x, shape=[-1, maxlen, embedding_size, 1])
+
+    # Convolution Layer
+    conv1 = conv2d(x, weights['wc1'], biases['bc1'])
+    # Max Pooling (down-sampling)
+    conv1 = maxpool2d(conv1, k=2)
+
+    # Convolution Layer
+    conv2 = conv2d(conv1, weights['wc2'], biases['bc2'])
+    # Max Pooling (down-sampling)
+    conv2 = maxpool2d(conv2, k=2)
+
+    # Fully connected layer
+    # Reshape conv2 output to fit fully connected layer input
+    fc1 = tf.reshape(conv2, [-1, weights['wd1'].get_shape().as_list()[0]])
+    fc1 = tf.add(tf.matmul(fc1, weights['wd1']), biases['bd1'])
+    fc1 = tf.nn.relu(fc1)
+    # Apply Dropout
+    fc1 = tf.nn.dropout(fc1, dropout)
+
+    # Output, class prediction
+    out = tf.add(tf.matmul(fc1, weights['out']), biases['out'])
+    return out
+
+# Store layers weight & bias
+weights = {
+    # 5x5 conv, 1 input, 32 outputs
+    'wc1': tf.Variable(tf.random_normal([5, 5, 1, 32])),
+    # 5x5 conv, 32 inputs, 64 outputs
+    'wc2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
+    # fully connected, 7*7*64 inputs, 1024 outputs
+    'wd1': tf.Variable(tf.random_normal([7*7*64, 1024])),
+    # 1024 inputs, 2 outputs (class prediction)
+    'out': tf.Variable(tf.random_normal([1024, num_classes]))
+}
+
+biases = {
+    'bc1': tf.Variable(tf.random_normal([32])),
+    'bc2': tf.Variable(tf.random_normal([64])),
+    'bd1': tf.Variable(tf.random_normal([1024])),
+    'out': tf.Variable(tf.random_normal([num_classes]))
+}
+
+# Construct model
+last_cnn_output = conv_net(inputs, weights, biases, keep_prob)
+one_batch_predict = forwardNN(last_cnn_output)[0]
+probability = tf.nn.softmax(one_batch_predict)
+l2_loss = forwardNN(last_cnn_output)[1]
 cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=one_batch_predict, labels=Y)) + 0.001 * l2_loss
 global_step = tf.Variable(0, trainable=False)
 starter_learning_rate = 0.1
